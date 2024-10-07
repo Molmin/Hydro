ac660b629c315469b00cbfa9e447d367c8ace976
Qi Zhu <115549630@qq.com>
2024-10-07 05:12:08 +0000

    updates

--- packages/hydrooj-countdown/index.ts
+++ packages/hydrooj-countdown/index.ts
@@ -1,10 +1,19 @@
-import { Context } from 'hydrooj';
+import { Context, Time } from 'hydrooj';
+
+interface CountdownData {
+    title: string;
+    max_dates: number;
+    dates: Array<{ name: string, date: Date }>;
+}
 
 export function apply(ctx: Context) {
     ctx.withHandlerClass('HomeHandler', (h) => {
-        h.prototype['getCountdown'] = (data) => {
-            console.info(data);
-            return data;
-        };
+        h.prototype['getCountdown'] = async (domainId: string, data: CountdownData) => ({
+            title: data.title,
+            dates: data.dates
+                .filter((item) => item.date > new Date())
+                .map((item) => ({ ...item, remaining: Math.ceil((item.date.getTime() - Date.now()) / Time.day) }))
+                .slice(0, data.max_dates),
+        });
     });
 }
--- packages/hydrooj-countdown/readme.md
+++ packages/hydrooj-countdown/readme.md
@@ -1,14 +1,16 @@
+配置示例：
+
 ```yaml
 countdown:
-  title: 倒计时
+  title: Countdown
   max_dates: 3
   dates:
     - name: IOI 2024
-      date: 2022-09-01
+      date: 2024-09-01
     - name: CSP-J/S 2024 第一轮认证
-      date: 2022-09-21
+      date: 2024-09-21
     - name: CSP-J/S 2024 第二轮认证
-      date: 2022-10-26
+      date: 2024-10-26
     - name: NOIP 2024
-      date: 2022-11-30
+      date: 2024-11-30
 ```
