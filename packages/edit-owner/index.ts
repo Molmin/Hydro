34a818657c0523e75062d068c5b5c3bd7fed2d24
Qi Zhu <115549630@qq.com>
2025-01-03 04:50:20 +0000

    edit_owner: init handler

---
+++ packages/edit-owner/index.ts
@@ -0,0 +1,24 @@
+import {
+    Context, Handler, post,
+    ProblemModel, ProblemNotFoundError, route,
+    Types, UserModel, UserNotFoundError,
+} from 'hydrooj';
+
+export class ProblemEditOwnerHandler extends Handler {
+    @route('pid', Types.ProblemId)
+    @post('owner', Types.Int)
+    async post(
+        domainId: string, pid: string | number, owner: number,
+    ) {
+        const pdoc = await ProblemModel.get(domainId, pid);
+        if (!pdoc) throw new ProblemNotFoundError(domainId, pid);
+        const udoc = await UserModel.getById(domainId, owner);
+        if (!udoc) throw new UserNotFoundError(domainId, owner);
+        await ProblemModel.edit(domainId, pdoc.docId, { owner });
+        this.back();
+    }
+}
+
+export function apply(ctx: Context) {
+    ctx.Route('problem_edit_owner', '/p/:pid/edit_owner', ProblemEditOwnerHandler, PRIV.PRIV_EDIT_SYSTEM);
+}
